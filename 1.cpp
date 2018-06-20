5ad1abd25e7599c35a1c73d3faf2c8a1eab9d196
Mswsxgz <37926064+Mswsxgz@users.noreply.github.com>
2018-06-21 04:04:03 +0800

    my homework

---
+++ 1.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+	float pai=3.1415926;
+	int r;
+	float s;
+	scanf ("%d",&r);
+	s=pai*r*r;
+	printf("园的面积为%f",s);
+	return 0;
+}
---
+++ 2.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main (){
+	int m=500;
+	printf("\"How many student here?\"\n");
+	printf("\"%d\"",m);
+	return 0;
+}
